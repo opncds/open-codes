ce73ce0ebb0398f107e4dff36395ca20b35e6ffc
mykola holovko <holovko.mykola@gmail.com>
2020-12-06 19:09:26 +0200

    2-2-3

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,9 +1,77 @@
 module.exports = {
-  "b-2-2-21": [
+  docs: [
+    {
+      type: "category",
+      label: "ДБН A.2.2-3 Склад та зміст проектної документації на будівництво",
+      collapsed: true,
+      items: [
+        "dbn-a-2-2-3/doc/index",
+        "dbn-a-2-2-3/doc/c01",
+        "dbn-a-2-2-3/doc/c02",
+        "dbn-a-2-2-3/doc/c03",
+        "dbn-a-2-2-3/doc/c04",
+        "dbn-a-2-2-3/doc/c05",
+        "dbn-a-2-2-3/doc/c06",
+        "dbn-a-2-2-3/doc/c07",
+        "dbn-a-2-2-3/doc/c08",
+        "dbn-a-2-2-3/doc/c09",
+        {
+          type: "category",
+          label: "Додатки",
+          collapsed: true,
+          items: [
+            "dbn-a-2-2-3/doc/a",
+            "dbn-a-2-2-3/doc/b",
+            "dbn-a-2-2-3/doc/c",
+            "dbn-a-2-2-3/doc/d",
+            "dbn-a-2-2-3/doc/e",
+            "dbn-a-2-2-3/doc/f",
+            "dbn-a-2-2-3/doc/g",
+            "dbn-a-2-2-3/doc/h",
+            "dbn-a-2-2-3/doc/i",
+            "dbn-a-2-2-3/doc/j",
+            "dbn-a-2-2-3/doc/k",
+          ],
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "ДБН В.1.1-7:2016 Пожежна безпека об’єктів будівництва",
+      collapsed: true,
+      items: [
+        "dbn-v-1-1-7/doc/index",
+        "dbn-v-1-1-7/doc/c01",
+        "dbn-v-1-1-7/doc/c02",
+        "dbn-v-1-1-7/doc/c03",
+        "dbn-v-1-1-7/doc/c04",
+        "dbn-v-1-1-7/doc/c05",
+        "dbn-v-1-1-7/doc/c06",
+        "dbn-v-1-1-7/doc/c07",
+        "dbn-v-1-1-7/doc/c08",
+        "dbn-v-1-1-7/doc/c09",
+        {
+          type: "category",
+          label: "Додатки",
+          collapsed: true,
+          items: [
+            "dbn-v-1-1-7/doc/a",
+            "dbn-v-1-1-7/doc/b",
+            "dbn-v-1-1-7/doc/c",
+            "dbn-v-1-1-7/doc/d",
+            "dbn-v-1-1-7/doc/e",
+            "dbn-v-1-1-7/doc/f",
+            "dbn-v-1-1-7/doc/g",
+            "dbn-v-1-1-7/doc/h",
+            "dbn-v-1-1-7/doc/i",
+          ],
+        },
+      ],
+    },
     {
       type: "category",
       label: "ДБН Б.2.2-12:2019 Планування і забудова територій",
-      collapsed: false,
+      collapsed: true,
       items: [
         "b-2-2-12/index",
         "b-2-2-12/c01",
@@ -16,20 +84,5 @@ module.exports = {
         "b-2-2-12/c08",
       ],
     },
-  ],
-  "v-1-1-7": [
-    {
-      type: "category",
-      label: "ДБН В.1.1-7:2016 Пожежна безпека об’єктів будівництва",
-      collapsed: false,
-      items: [
-        "dbn-v-1-1-7/doc/index",
-        "dbn-v-1-1-7/doc/c01",
-        "dbn-v-1-1-7/doc/c02",
-        "dbn-v-1-1-7/doc/c03",
-        "dbn-v-1-1-7/doc/c04",
-        "dbn-v-1-1-7/doc/c05",
-      ],
-    },
-  ],
-};
+  ]
+}
--- websitess/sidebars.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    "b-2-2-12": {
-        Docusaurus: ['c01', 'c02', 'c03'],
-        Features: ['mdx'],
-    },
-};
