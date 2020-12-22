b9c02788f4b052576b0580662cabd3e051cc7431
Herman Mitish <german.mitish@gmail.com>
2020-12-22 17:46:22 +0200

    Title page

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -6,135 +6,123 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons'
-
-const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
-const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${wrap ? ')' : ''}`
-const Pink = ({ children }) => <span style={{ color: '#FF6AC1' }}>{children}</span>
-const Yellow = ({ children }) => <span style={{ color: '#EFF59B' }}>{children}</span>
-const Lightblue = ({ children }) => <span style={{ color: '#9AEDFE' }}>{children}</span>
-const Green = ({ children }) => <span style={{ color: '#57EE89' }}>{children}</span>
-const Blue = ({ children }) => <span style={{ color: '#57C7FF' }}>{children}</span>
-const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>
-
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   const parallax = useRef()
-  
+  const desktop = useMediaQuery('(min-width:900px)');
+
   return (
     <Layout
+    className='noscrollbar'
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <main style={{ backgroundColor:'#363e52' }}>
-        <Parallax ref={parallax} pages={3} style={{ backgroundColor:'#363e52' }}>
+      <main style={{ backgroundColor:'#363e52' }} className='noscrollbar'>
+        <Parallax ref={parallax} className='noscrollbar' pages={7} style={{ backgroundColor:'#363e52' }}>
           <ParallaxLayer 
+          className='noscrollbar'
+            offset={0} 
+            speed={0} 
+            style={{ backgroundColor:'#363e52', zIndex:0, backgroundSize: 'cover' }}
+          ></ParallaxLayer>
+
+          <ParallaxLayer 
+          className='noscrollbar'
             offset={0} 
             speed={1} 
             onClick={() => parallax.scrollTo(1)}
-            style={{ backgroundColor:'#363e52', zIndex:10, backgroundSize: 'cover' }}
+            style={{
+              backgroundImage: "url(https://d2kq0urxkarztv.cloudfront.net/5443d73367f3ed093ea65c73/1806316/upload-1b9524a1-9c21-4c44-b020-1bba0b4aa065.jpg?w=3617&e=webp)" ,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center'
+            }}
           >
-            <img srcSet="https://d2kq0urxkarztv.cloudfront.net/5443d73367f3ed093ea65c73/1806316/upload-1b9524a1-9c21-4c44-b020-1bba0b4aa065.jpg?w=3617&amp;e=webp 2x, https://d2kq0urxkarztv.cloudfront.net/5443d73367f3ed093ea65c73/1806316/upload-1b9524a1-9c21-4c44-b020-1bba0b4aa065.jpg?e=webp 3x" src="https://d2kq0urxkarztv.cloudfront.net/5443d73367f3ed093ea65c73/1806316/upload-1b9524a1-9c21-4c44-b020-1bba0b4aa065.jpg?w=3617&amp;e=webp" className="viewable" style={{opacity: 1}}/>
 
           </ParallaxLayer>
-          <ParallaxLayer offset={0} speed={1}>
-            <div className="text-viewer" style={{zIndex:12}}>
-              <p style={{lineHeight:"95px", zIndex:12}}>
+          <ParallaxLayer offset={0} speed={0.5} className='noscrollbar'>
+            <div 
+              className="text-viewer" 
+              style={{
+                maxWidth:'90%',
+                width:'fit-content',
+                paddingLeft:'10%',
+                paddingTop: '30vh'
+              }}
+            >
+              <p style={{lineHeight:"45px"}}>
                 <span style={{
-                  zIndex:12,
-                  fontSize:"76px",
-                  fontWeight:600,
-                  fontFamily:'ysxc',
+                  fontSize:"3.5rem",
+                  fontSize: desktop?"6vw":"3.5rem",
+                  fontWeight: 600,
                   fontStyle: 'normal',
                   textDecoration: 'none', 
                   textTransform: 'none',
-                  color:'rgba(255, 255, 255, 1)'
+                  display:'block'
                 }}>
                   Open Codes
+                </span>
+              </p>
+              <p style={{lineHeight:desktop?"25px":"0px"}}>
+                <span 
+                  style={{
+                    fontSize:'0.8rem',
+                    fontSize: desktop?"1.35vw":"0.8rem",
+                    fontWeight:600,
+                    letterSpacing:'0.4px',
+                    textDecoration:'none',
+                    color:'rgba(147, 165, 200, 1)',
+                    display:'block',
+                    float:'right'
+                  }}
+                >
+                  Відкриті Будівельні Норми
+                </span>
+              </p>
+              <p style={{paddingTop:'5vw'}}>
+                <span 
+                  style={{
+                    fontSize:'1.4rem',
+                    fontSize: desktop?"1.5vw":"1rem",
+                    fontWeight:600,
+                    letterSpacing:'0.4px',
+                    textDecoration:'none',
+                    display:'block',
+                  }}
+                >
+                  Онлайн платформа для зручної 
+                </span>
+                <span 
+                  style={{
+                    fontSize:'1.4rem',
+                    fontSize: desktop?"1.5vw":"1rem",
+                    fontWeight:600,
+                    letterSpacing:'0.4px',
+                    textDecoration:'none',
+                    display:'block',
+                  }}
+                >
+                  роботи з будівельними нормами
+                </span>
+                <span 
+                  style={{
+                    width:'10vw',
+                    paddingTop:'1rem',
+                    display:'block',
+                  }}
+                >
+                  <img 
+                    srcset="https://d2kq0urxkarztv.cloudfront.net/5443d73367f3ed093ea65c73/1806316/upload-adae9722-1bfa-4b52-a4e9-6b2eaf5989af.png?w=285&amp;e=webp&amp;nll=true 2x, https://d2kq0urxkarztv.cloudfront.net/5443d73367f3ed093ea65c73/1806316/upload-adae9722-1bfa-4b52-a4e9-6b2eaf5989af.png?e=webp&amp;nll=true 3x" 
+                    src="https://d2kq0urxkarztv.cloudfront.net/5443d73367f3ed093ea65c73/1806316/upload-adae9722-1bfa-4b52-a4e9-6b2eaf5989af.png?w=285&amp;e=webp&amp;nll=true" 
+                    className="viewable" 
+                    style={{opacity: 1}}
+                  />
                 </span>
               </p>
             </div>
           </ParallaxLayer>
 
-          {/* <ParallaxLayer offset={0} speed={0} factor={3} style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }} /> */}
-
-          <ParallaxLayer 
-            offset={0} 
-            speed={0} 
-            style={{ backgroundColor:'#363e52', zIndex:0, backgroundSize: 'cover' }}
-          ></ParallaxLayer>
-          <ParallaxLayer
-            offset={2}
-            speed={-0.3}
-            style={{
-              backgroundSize: '80%',
-              backgroundPosition: 'center',
-              backgroundImage: url('clients', true)
-            }}
-          />
-
-          <ParallaxLayer
-            offset={1}
-            speed={0.1}
-            onClick={() => parallax.scrollTo(2)}
-            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <img src={url('bash')} style={{ width: '40%' }} />
-          </ParallaxLayer>
-
-          <ParallaxLayer
-            offset={2}
-            speed={-0}
-            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-            onClick={() => this.parallax.scrollTo(0)}>
-            <img src={url('clients-main')} style={{ width: '40%' }} />
-          </ParallaxLayer>
         </Parallax>
       </main>
     </Layout>
